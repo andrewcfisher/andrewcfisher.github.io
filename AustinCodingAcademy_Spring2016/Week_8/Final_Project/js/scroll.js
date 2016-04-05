4d5babefb90c9c547b797bcad03ee01d4b321b22
andrewcfisher <andrewcfisher1@gmail.com>
2016-04-05 14:39:16 -0500

    final project p moves, violence image change, js not working

--- AustinCodingAcademy_Spring2016/Week_8/Final_Project/js/scroll.js
+++ AustinCodingAcademy_Spring2016/Week_8/Final_Project/js/scroll.js
@@ -1,19 +1,57 @@
-$(document).ready(function(){
-    
-$(window).scroll(function() {
-        
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+
         var verticalScroll = $(this).scrollTop();
-        
-        if(verticalScroll > 100) {
-            
+
+        if (verticalScroll > 100) {
+
             $('.the_encounter').addClass('animated fadeIn');
-            
             $('.the_encounter').removeClass('hide_me');
-            
+
+
         }
-        
+
     });
 
+    $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll > 400) {
+
+            $('.second_p').addClass('animated fadeIn');
+            $('.second_p').removeClass('hide_me');
+
+        }
+
+    });
+
+    $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll > 1200) {
+
+            $('#sensitive').addClass('animated fadeIn');
+            $('#sensitive').removeClass('hide_me');
+
+
+        }
+
+    });
+
+    $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll > 1600) {
+            $('#intention').addClass('animated fadeIn');
+            $('#intention').removeClass('hide_me');
+
+        }
+
+    });
+
+
 });
-    
-   
