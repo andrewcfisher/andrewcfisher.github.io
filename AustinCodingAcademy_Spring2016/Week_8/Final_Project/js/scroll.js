9d8b56df640041be638da182e81a377c03409224
andrewcfisher <andrewcfisher1@gmail.com>
2016-04-07 01:11:20 -0500

    Final Project- one million media queries

--- AustinCodingAcademy_Spring2016/Week_8/Final_Project/js/scroll.js
+++ AustinCodingAcademy_Spring2016/Week_8/Final_Project/js/scroll.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
         var verticalScroll = $(this).scrollTop();
 
-        if (verticalScroll > 200) {
+        if (verticalScroll > 150) {
 
             $('.the_encounter').addClass('animated fadeIn');
             $('.the_encounter').removeClass('hide_me');
@@ -27,31 +27,6 @@ $(document).ready(function () {
 
     });
 
-    $(window).scroll(function () {
-
-        var verticalScroll = $(this).scrollTop();
-
-        if (verticalScroll > 1800) {
-
-            $('#sensitive').addClass('animated fadeIn');
-            $('#sensitive').removeClass('hide_me');
-
-
-        }
-
-    });
-
-    $(window).scroll(function () {
-
-        var verticalScroll = $(this).scrollTop();
-
-        if (verticalScroll > 1900) {
-            $('#intention').addClass('animated fadeIn');
-            $('#intention').removeClass('hide_me');
-
-        }
-
-    });
     
     //smooth scroll try
     
